393d3dc2d096747ce123bd474a3c1b15d8409f71
TolotrAA <62598155+TolotrAA@users.noreply.github.com>
2021-05-17 22:28:26 +0200

    Initial commit

---
+++ WEB/js/main.js
@@ -0,0 +1,63 @@
+
+const openMenu = document.querySelector(".openMenu"); 
+const closeMenu = document.querySelector(".closeMenu");
+const mainMenu = document.querySelector(".mainMenu");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+function close() {
+  mainMenu.style.top = "-100%";
+}
+
+/* Slider*/
+
+var slideIndex = 1;
+showSlides1(slideIndex);
+
+// Suivant/précédent
+function plusSlides(n) {
+  showSlides1(slideIndex += n);
+}
+
+// Control Image
+function currentSlide(n) {
+  showSlides1(slideIndex = n);
+}
+
+function showSlides1(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+/* Mode AUTO */
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // L'image change toute les 5 sec
+}
+/* End Slider*/
